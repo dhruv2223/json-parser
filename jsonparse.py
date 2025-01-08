26884b19a3e2869b8ed74ad47fc9d8b275baac06
dhruv2223 <DHRUV.CHOTALIA017@svkmmumbai.onmicrosoft.com>
2025-01-08 18:49:39 +0530

    added string parsing in lexer

--- jsonparse.py
+++ jsonparse.py
@@ -27,11 +27,44 @@ class Lexer:
             elif char.isalpha(): 
                 literal = self.read_literal(char)
                 tokens.append({"type":"LITERAL","value":literal})
+            elif char == '"':
+                string_value = self.read_string(char)
+                tokens.append({"type":"STRING","value":string_value})
+                
+                
             else:
                 raise ValueError(f"Invalid Character: {char}")
         return tokens
-
-
+    def read_string(self,char):
+        string_value = ""
+        while True:
+            char = self.get_next_char()
+            if char=='"':
+                break
+            if char == '\\':  
+                escape_char = self.get_next_char()
+                if escape_char == '"':
+                    string_value += '"'
+                elif escape_char == '\\':
+                    string_value += '\\'
+                elif escape_char == '/':
+                    string_value += '/'
+                elif escape_char == 'b':
+                    string_value += '\b'
+                elif escape_char == 'f':
+                    string_value += '\f'
+                elif escape_char == 'n':
+                    string_value += '\n'
+                elif escape_char == 'r':
+                    string_value += '\r'
+                elif escape_char == 't':
+                    string_value += '\t'
+                else:
+                    raise ValueError(f"Invalid escape sequence: \\{escape_char}")
+                
+            else:
+                string_value +=char
+        return string_value
 
     def read_literal(self,char):
         literal = char 
@@ -46,8 +79,4 @@ input_json = '{"key": true, "value": null}'
 lexer = Lexer(input_json)
 tokens = lexer.tokenize()
 for token in tokens:
-    print(token)
-                
-                
-    
-            
+    print(token) 
